678b35b87335199dc15b2cfa32a96d038650df7a
Alex Okrushko <alex@ts.dev>
2021-02-07 17:36:15 -0500

    interactive menu (#15)

--- js/guide-menu.js
+++ js/guide-menu.js
@@ -5,22 +5,61 @@ function toggle(button) {
 function closeSideNav() {
     document.getElementById("guide-nav-button")?.classList.remove("active");
 }
-document.querySelectorAll('h2, h3, h4').forEach((header) => {
-    const link = document.createElement("a");
-    link.href = "#" + header.id;
-    link.classList.add('clipboard');
-    link.innerText = "📋";
-    link.addEventListener('click', (event) => {
-        event.preventDefault(); // do not navigate on click.
-        navigator.clipboard.writeText(link.href).then(() => {
-            const toast = document.createElement('span');
-            toast.classList.add('clipboard-confirm');
-            toast.innerText = "Copied";
-            link.append(toast);
-            setTimeout(() => {
-                toast.remove();
-            }, 1000);
+window.addEventListener("load", loaded);
+function loaded() {
+    const options = {
+        root: document.querySelector(".sg-container"),
+        rootMargin: `0px 0px -90% 0px`,
+        threshold: 0.5,
+    };
+    const tocElement = document.querySelector(".toc");
+    const tocAnchors = tocElement.querySelectorAll("a");
+    const headers = document.querySelectorAll("h2, h3, h4");
+    tocAnchors.forEach((anchor) => {
+        anchor.addEventListener("click", () => {
+            tocAnchors.forEach((other) => {
+                if (other.classList.contains("active")) {
+                    other.classList.remove("active");
+                }
+            });
+            anchor.classList.add("active");
         });
     });
-    header.append(link);
-});
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const href = entry.target.getElementsByTagName("a")[0].href;
+                for (const navItem of tocAnchors) {
+                    if (navItem.classList.contains("active")) {
+                        navItem.classList.remove("active");
+                    }
+                    if (navItem.href === href) {
+                        navItem.classList.add("active");
+                        navItem.scrollIntoView({ behavior: "smooth", block: "center" });
+                        history.pushState({}, document.title, href);
+                    }
+                }
+            }
+        });
+    }, options);
+    headers.forEach((header) => {
+        observer.observe(header);
+        const link = document.createElement("a");
+        link.href = "#" + header.id;
+        link.classList.add("clipboard");
+        link.innerText = "📋";
+        link.addEventListener("click", (event) => {
+            event.preventDefault(); // do not navigate on click.
+            navigator.clipboard.writeText(link.href).then(() => {
+                const toast = document.createElement("span");
+                toast.classList.add("clipboard-confirm");
+                toast.innerText = "Copied";
+                link.append(toast);
+                setTimeout(() => {
+                    toast.remove();
+                }, 1000);
+            });
+        });
+        header.append(link);
+    });
+}
